3ad592e2456849eec7755527c0cf6fcb98f7a239
tyronus4 <65735262+tyronus4@users.noreply.github.com>
2021-11-03 21:37:52 -0700

    Finished Calculator Java Program
    Finished adding in calculator methods and parsing methods

--- calculator.java
+++ calculator.java
@@ -5,121 +5,11 @@ import java.lang.String;
 import java.lang.Character;
 
 public class calculator {
-
-   private static double Add(double input, double input2) {
    
-   double c = input+input2;
-   return c;
-   
-   }
-   
-   //Sub method inverts the value of input2 and adds it to input.
-   //This accomodates for both unary and binary operations
-   //make sure this works well!
-   private static double Sub(double input) {
-   
-   
-   double c = input*-1;
-   return c;
-   
-   }
-   
-   private static double Multi(double input, double input2) {
-   
-   double c = input*input2;
-   return c;
-   
-   }
-   
-   private static double Divide(double input, double input2) {
-   
-   double c = input/input2;
-   return c;
-   
-   }
-   
-   private static double Exponent(double input, double input2) {
-   
-   double c = Math.pow(input, input2);
-   return c;
-   
-   }
-   
-   private static double logn(double input) {
-   
-   double c = Math.log(input);
-   
-   return c;
-   
-   }
-   
-   private static double log10(double input) {
-   
-   double c = Math.log10(input);
-   
-   return c;
-   
-   }
-   
-   //check these methods if they should automatically convert input to radians
-   //Have a check if the value is between -1 and 1
-   //Nope just do degrees only, include that with the read me
-   private static double Sin(double input) {
-   
-   double c = Math.toRadians(input);
-   c = Math.sin(c);
-   
-   return c;
-   
-   }
-   
-   private static double Cos(double input) {
-   
-   double c = Math.toRadians(input);
-   c = Math.cos(c);
-   
-   return c;
-   
-   }
-   
-   private static double Tan(double input) {
-   
-   double c = Math.toRadians(input);
-   c = Math.tan(c);
-   
-   return c;
-   
-   }
-   
-   private static double ASin(double input) {
-   
-   double c = Math.toRadians(input);
-   c = Math.asin(c);
-   
-   return c;
-   
-   }
-   
-   private static double ACos(double input) {
-   
-   double c = Math.toRadians(input);
-   c = Math.acos(c);
-   
-   return c;
-   
-   }
-   
-   private static double ATan(double input) {
-   
-   double c = Math.toRadians(input);
-   c = Math.atan(c);
-   
-   return c;
-   
-   }
-   
+   //This method goes through the string and replaces any operator or parentheses with spaces to aid in 
+   //formatting for RPN
    public static String replacing(String s) {
-   String[] chars = {"+", "-", "/", "^", "(", ")", "[", "]", "{", "}"};
+   String[] chars = {"+", "-", "/", "^", "sqrt", "(", ")", "[", "]", "{", "}"};
 
    for (String character : chars) {
       if (s.contains(character)) {
@@ -127,8 +17,9 @@ public class calculator {
       }
    }
    return s;
-   }   
+   }
    
+   //Gets the precedence for operators from the string, assigns a int value used later
    public static int precedence(String c) {
       
       if (c.equals("+") || c.equals("-")){
@@ -137,23 +28,31 @@ public class calculator {
       else if (c.equals("*") || c.equals("/")){
          return 2;
       }
-      else if (c.equals("^")){
+      else if (c.equals("^") || c.equals("sqrt")){
          return 3;
       }
       else
          return -1;
    }
    
+   //Checks if the string is an open/close parentheses, square bracket, or curly brace
+   //Also checks if it is a number with associated parentheses, i.e. Sin(), Cos(), ln()
+   //assigns a int value accordingly
    public static int bracket(String c) {
    
       if (c.equals("(") || c.equals(")") || c.equals("[") || c.equals("]") || c.equals("{") || c.equals("}")){
          return 4;
       }
+      else if(c.equals("ln") || c.equals("log") || c.equals("sin") || c.equals("cos") ||
+      c.equals("tan") || c.equals("arcsin") || c.equals("arccos") || c.equals("arctan")) {
+         return 5;
+      }
+      
       else
          return -1;
       }
    
-   
+   //RPN method. Goes through and converts the string into Reverese polish notation order
    public static String form(String expression) {
    
    Stack<String> stack = new Stack<>();
@@ -167,11 +66,11 @@ public class calculator {
       String k = split[i];
    
       if(precedence(k) == -1 && bracket(k) == -1){
-         output = output+k;
+         output = output+k+" ";
       }
       
       //following section goes through and checks for paranthesis, square brackets and curly braces
-      else if(k.equals("(")) {
+      else if(k.equals("(") || k.equals("[") || k.equals("{")) {
          stack.push(k);
       }
       else if(k.equals(")")) {
@@ -179,24 +78,31 @@ public class calculator {
             output = output + stack.pop();
          }
          stack.pop();
-      }
-      else if(k.equals("[")) {
-         stack.push(k);
+         if(bracket(stack.peek()) == 5){
+            output = output + stack.pop();
+         }
       }
       else if(k.equals("]")) {
          while(!stack.isEmpty() && !stack.peek().equals("[")) {
             output = output + stack.pop();
          }
          stack.pop();
-      }
-      else if(k.equals("{")) {
-         stack.push(k);
+         if(bracket(stack.peek()) == 5){
+            output = output + stack.pop();
+         }
       }
       else if(k.equals("}")) {
          while(!stack.isEmpty() && !stack.peek().equals("{")) {
             output = output + stack.pop();
          }
          stack.pop();
+         if(bracket(stack.peek()) == 5){
+            output = output + stack.pop();
+         }
+      }
+      //all trig functions act as extra parenthesis, push to stack if present and then pop if at top of stack again
+      else if(bracket(k) == 5) {
+         stack.push(k); 
       }
       
       else{
@@ -209,7 +115,7 @@ public class calculator {
    }
    
    while(!stack.isEmpty()) {
-      if(stack.peek().equals("(") || stack.peek().equals("[") || stack.peek().equals("{")){
+      if(stack.peek().equals("(") || stack.peek().equals("[") || stack.peek().equals("{") || bracket(stack.peek()) == 5){
          return "This is not a valid expression";
       }
       output = output + stack.pop();
@@ -219,39 +125,133 @@ public class calculator {
    }
    
    
-  
    
+   //method to calculate the total of the given expression
+   public static double calculation(String expression) {
    
+   double input1 = 0;
+   double input2 = 0;
+   Stack<Double> total = new Stack<>();
+
+   //fixing the strings whitespaces    
+   expression = replacing(expression);
+   expression = expression.replaceAll("\\s+", " ");
+
    
+   String[] split = expression.split(" ");
    
+   //goes through the string and does the necessary operations
+   //in RPN order.
+   for (int i = 0; i<split.length; i++) {
    
+      String k = split[i];
+      
+      if(k.equals("+")) {
+         input1 = total.pop();
+         input2 = total.pop();
+         total.push(input1+input2);
+      }
+      else if(k.equals("-")) {
+         input1 = total.pop();
+         total.push(input1*-1);
+      }
+      else if(k.equals("*")) {
+         input1 = total.pop();
+         input2 = total.pop();
+         total.push(input1*input2);
+      }
+      else if(k.equals("/")) {
+         input1 = total.pop();
+         input2 = total.pop();
+         total.push(input2/input1);
+      }
+      else if(k.equals("^")) {
+         input1 = total.pop();
+         input2 = total.pop();
+         total.push(Math.pow(input2, input1));
+      }
+      else if(k.equals("sqrt")) {
+         input1 = total.pop();
+         total.push(Math.sqrt(input1));
+      }
+      else if(k.equals("ln")) {
+         input1 = total.pop();
+         total.push(Math.log(input1));
+      }
+      else if(k.equals("log")) {
+         input1 = total.pop();
+         total.push(Math.log10(input1));
+      }
+      else if(k.equals("sin")) {
+         input1 = total.pop();
+         input1 = Math.toRadians(input1);
+         total.push(Math.sin(input1));
+      }
+      else if(k.equals("cos")) {
+         input1 = total.pop();
+         input1 = Math.toRadians(input1);
+         total.push(Math.cos(input1));
+      }
+      else if(k.equals("tan")) {
+         input1 = total.pop();
+         input1 = Math.toRadians(input1);
+         total.push(Math.tan(input1));
+      }
+      else if(k.equals("arcsin")) {
+         input1 = total.pop();
+         input1 = Math.toRadians(input1);
+         total.push(Math.asin(input1));
+      }
+      else if(k.equals("arccos")) {
+         input1 = total.pop();
+         input1 = Math.toRadians(input1);
+         total.push(Math.acos(input1));
+      }
+      else if(k.equals("arctan")) {
+         input1 = total.pop();
+         input1 = Math.toRadians(input1);
+         total.push(Math.atan(input1));
+      }                                          
+      else {
+         try {
+            total.push(Double.parseDouble(k));
+         }
+         catch (NumberFormatException nfe) {
+            System.out.println("An invalid value was attempted to be parsed: "+ k);
+            System.out.println("Please correct the given mathematical expression");
+         }
+         
+      }                            
    
+   }
+   
+   input1 = 0;
+   
+   while(!total.isEmpty()){
+      input1 += total.pop();
+   }
+   
+      
+   return input1;
+   
+   }
 
 
-
-   public static void main(String[] arg) {
+   //main method, takes in command line argument and parses it. 
+   //Needs to be a single string, no spaces allowed for this implementation
+   public static void main(String[] args) {
    
-
-   //Shunting yard works, need to tell numbers with decimals apart from each other
-   //how about making all numbers with decimals end with double 0 (00)? Easily detectable
-   //unary opertors suck
-   //have minus be multiply by -1, then at the end of the expression add all remaining elements together
-   //a negative comes right after a negative, add the two previous elements together. If a negative follows a positive,
-   //negate and add the elements together (figure this out a bit more)
+   String answer = args[0];
+   answer = form(answer);
+   if (answer.equals("This is not a valid expression")) {
+      System.out.println("An invalid value was attempted to be parsed");
+      System.out.println("Please correct the given mathematical expression");
+   }
+   else {
+      double output = calculation(answer);
+      System.out.println(output);
+   }
    
-   
-   String test = "sin(30)+100+350.5900/3^2-(-5+log(3))";
-   System.out.println(form("5+2/(3-8)^5^2"));
-   test = form(test);
-   System.out.println(test);
-//    String[] split = test.split(" ");
-//    System.out.println(test);
-//    for (int i = 0; i< split.length; i++) {
-//    
-//    
-//       System.out.println(split[i]);
-//    
-//       }
    }
 
 
